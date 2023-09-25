cca191b8ac32a9f877c93a77f6af50253f38a105
antogf95 <antonella_gandolfi@hotmail.com>
2023-09-25 15:15:49 -0300

    se agrega contact

--- pruebaclase1/views.py
+++ pruebaclase1/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from form
 # Create your views here.
 
 
 def usuarios(request):
     return render(request, "pruebaclase1/usuarios.html")
+
+def contact(request):
+    if request.method == 'POST':
+        name= request.POST.get('nombre', '')
+        return HttpResponse(f"el nombre del formulario es {name}")
+    return render (request, 'pruebaclase1/contact.html')
